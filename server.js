8a5c5fabd8eb062f446f6e9335732b21d41fe813
Aphisit Likitwattanapaisarn <aphisit.heung@gmail.com>
2022-03-14 19:50:43 +0700

    try some more idea .

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.use(express.static("public"));
 
 // get a random quote from quotes array object.
 app.get("/api/quotes/random", (req, res) => {
-  res.send(getRandomElement(quotes));
+  res.send({
+    quote: getRandomElement(quotes),
+  });
 });
 
 // get a params person to find which quotes have the same person name if not it will send empty array.
@@ -22,6 +24,50 @@ app.get("/api/quotes", (req, res) => {
   res.send({
     quotes: result,
   });
+});
+
+// receive data from query and check if that both exists if true it will add to quotes
+// otherwise it will send back status 400
+app.post("/api/quotes", (req, res) => {
+  const data = req.query;
+  if (data.quote && data.person) {
+    quotes.push(data);
+    res.status(201).send({
+      quote: data,
+    });
+  } else {
+    res.status(400).send();
+  }
+});
+
+// receive param index and data that want to replace from query
+// find that quotes with index if found we will replace them with data otherwise send status 404!
+app.put("/api/quotes/:index", (req, res) => {
+  const index = Number(req.params.index);
+  const foundData = quotes[index];
+  const updateData = req.query;
+  if (foundData) {
+    quotes[index] = {
+      ...foundData,
+      ...updateData,
+    };
+    res.send(quotes[index]);
+  } else {
+    res.status(404).send();
+  }
+});
+
+// receive param index and find that quotes with index
+// if found we will remove them and send back status 204 otherwise send status 404!
+app.delete("/api/quotes/:index", (req, res) => {
+  const index = Number(req.params.index);
+  const foundData = quotes[index];
+  if (foundData) {
+    quotes.splice(index, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
 });
 
 app.listen(PORT, () => {
