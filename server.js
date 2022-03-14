8a27ea9093840c932a7124a789df050181dfd8da
Aphisit Likitwattanapaisarn <aphisit.heung@gmail.com>
2022-03-14 18:28:47 +0700

    Updated api path

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static("public"));
 
+// get a random quote from quotes array object.
 app.get("/api/quotes/random", (req, res) => {
   res.send(getRandomElement(quotes));
 });
 
+// get a params person to find which quotes have the same person name if not it will send empty array.
+// otherwise if we didn't get any params person it will send all quotes back.
 app.get("/api/quotes", (req, res) => {
-  res.send(quotes);
+  const result = req.query.person ? quotes.filter((quote) => quote.person === req.query.person) : quotes
+  res.send(result);
 });
 
 app.listen(PORT, () => {
